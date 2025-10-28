043f1d3a4c5ce514ab425878a34d071b7d0c3321
sundaysjohns78 <sundaysjohns@gmail.com>
2025-10-28 14:13:15 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,11 @@
-// firebase-config.js
 const firebaseConfig = {
   apiKey: "AIzaSyDLlalysOEMhmrUHxUUS8ZGDk6BEDwyIto",
   authDomain: "record-djt.firebaseapp.com",
   projectId: "record-djt",
-  storageBucket: "record-djt.firebasestorage.app",
+  storageBucket: "record-djt.appspot.com", // ✅ fixed
   messagingSenderId: "907107906667",
-  appId: "1:907107906667:web:ec2e751cedaeb98396ad2e"
+  appId: "1:907107906667:web:ec2e751cedaeb98396ad2e",
+  measurementId: "G-ESQL3DF4W1"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
